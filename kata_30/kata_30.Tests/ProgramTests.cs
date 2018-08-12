7c92f8d49d1eee25338411fa25409da3beb509d4
Alice Chen Sz-Ying <alice.chen56@titansoft.com.sg>
2018-08-12 22:38:21 +0800

    add kata_30 and ignore file

---
+++ kata_30/kata_30.Tests/ProgramTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using kata_30;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kata_30.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void MxdiflgTest()
+        {
+            string[] s1 = new string[] { "hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz" };
+            string[] s2 = new string[] { "cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww" };
+            Assert.AreEqual(13, Program.Mxdiflg(s1, s2));
+        }
+
+        [TestMethod()]
+        public void MxdiflgTest_Empty()
+        {
+            string[] s1 = new string[] { "hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz" };
+            string[] s2 = new string[] { };
+            Assert.AreEqual(-1, Program.Mxdiflg(s1, s2));
+        }
+
+        [TestMethod()]
+        public void MxdiflgTest_SameValue()
+        {
+            string[] s1 = new string[] { "hoqq", "bbllkw", "oox" };
+            string[] s2 = new string[] { "hoqq", "bbllkw", "oox" };
+            Assert.AreEqual(3, Program.Mxdiflg(s1, s2));
+        }
+    }
+}
---
+++ kata_30/kata_30/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kata_30
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s1 = new string[] { "hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz" };
+            string[] s2 = new string[] { "cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww" };
+
+            Program.Mxdiflg(s1, s2);
+        }
+
+        public static int Mxdiflg(string[] a1, string[] a2)
+        {
+            if (a1.Length != 0 && a2.Length != 0)
+            {
+                var result1 = a1.Max(x => x.Length) - a2.Min(x => x.Length);
+                var result2 = a2.Max(x => x.Length) - a1.Min(x => x.Length);
+                return result1 > result2 ? result1 : result2;
+            }
+            else
+            {
+                return -1;
+            }
+
+
+
+        }
+    }
+}
