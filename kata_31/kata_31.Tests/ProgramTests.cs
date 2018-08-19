0cf81e338c41dcfe3c5f3398d8a0190f6eab4365
Alice Chen Sz-Ying <alice.chen56@titansoft.com.sg>
2018-08-19 17:37:26 +0800

    add kata_31

---
+++ kata_31/kata_31.Tests/ProgramTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using kata_31;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kata_31.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void EncodeTest()
+        {
+            CollectionAssert.AreEqual(new List<int> { 20, 12, 18, 30, 21 }, Program.Encode("scout", 1939));
+            CollectionAssert.AreEqual(new List<int> { 14, 10, 22, 29, 6, 27, 19, 18, 6, 12, 8 }, Program.Encode("masterpiece", 1939));
+        }
+
+        [TestMethod()]
+        public void CharToIndexOfValue()
+        {
+            char[] testStrArr = {'s', 'c', 'o', 'u', 't'};
+            CollectionAssert.AreEqual(new List<int> { 19,3,15,21,20 }, Program.GetIndexOfValue(testStrArr));
+        }
+
+
+    }
+}
---
+++ kata_31/kata_31/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kata_31
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "scout";
+            Encode(input,1939);
+        }
+
+        public static List<int> Encode(string str,int n)
+        {
+            var inputStr = GetIndexOfValue(str.ToCharArray());
+            var inputNum = n.ToString().Select(x => int.Parse(x.ToString())).ToList();
+            return GetEncodeResult(inputStr,inputNum);
+        }
+
+        private static List<int> GetEncodeResult(List<int> arr,List<int> num)
+        {
+            //19,3,15,21,20
+            List<int> resultArr = new List<int>();
+            int index = 0;
+
+            foreach (var item in arr)
+            {
+                resultArr.Add(item + num[index % 4]);
+                index++;
+            }
+            return resultArr;
+
+        }
+
+        public static List<int> GetIndexOfValue(char[] str)
+        {
+            char[] enArr = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+            List<int> intValue = new List<int>();
+            foreach (char item in str)
+            {
+                intValue.Add(Array.IndexOf(enArr, item) + 1);
+            }
+            return intValue;
+        }
+    }
+}
