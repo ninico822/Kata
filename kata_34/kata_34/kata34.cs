efd409b949b612d5fd412e19ab7d45b6a902095f
TITANSOFT\alice.chen56 <alice.chen56@titansoft.com.sg>
2018-09-10 22:08:27 +0800

    add kata_34

---
+++ kata_34/kata_34/kata34.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+
+namespace kata_34
+{
+    public class kata34
+    {
+        public static int Persistence(long n)
+        { 
+            var times = 0;
+            while (n/10 > 0)
+            {
+                n = n.ToString().ToCharArray().ToList().Select(x=> (int)Char.GetNumericValue(x)).Aggregate((total, next) => total * next);
+                times++;
+            }
+            return times;
+        }
+    }
+}
---
+++ kata_34/kata_34Tests/kata34Tests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using kata_34;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kata_34.Tests
+{
+    [TestClass()]
+    public class kata34Tests
+    {
+        [TestMethod()]
+        public void PersistenceTest()
+        {
+            Assert.AreEqual(3, kata34.Persistence(39));
+            Assert.AreEqual(0, kata34.Persistence(4));
+            Assert.AreEqual(2, kata34.Persistence(25));
+            Assert.AreEqual(4, kata34.Persistence(999));
+        }
+    }
+}
