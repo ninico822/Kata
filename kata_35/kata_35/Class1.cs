5f5c9b6c509f27c88e068ca3955f06972dee683b
TITANSOFT\alice.chen56 <alice.chen56@titansoft.com.sg>
2018-09-24 21:50:24 +0800

    add kata_35

---
+++ kata_35/kata_35/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace kata_35
+{
+    public class Class1
+    {
+        public static int GetVowelCount(string str)
+        {
+            var strList = new List<char> { 'a', 'e', 'i', 'o', 'u' };
+            return str.ToCharArray().Count(x => strList.Contains(x));
+        }
+    }
+}
---
+++ kata_35/kata_35Tests/Class1Tests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace kata_35.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void GetVowelCountTestNormally()
+        {
+            Assert.AreEqual(5, Class1.GetVowelCount("abracadabra"));
+        }
+
+        [TestMethod()]
+        public void SpaceStringTest()
+        {
+            Assert.AreEqual(5, Class1.GetVowelCount("ab rac ada bra"));
+        }
+
+        [TestMethod()]
+        public void AllVowelStringTest()
+        {
+            Assert.AreEqual(13, Class1.GetVowelCount("aeiouioeaioue"));
+        }
+
+        [TestMethod()]
+        public void NoneStringTest()
+        {
+            Assert.AreEqual(0, Class1.GetVowelCount(" "));
+        }
+    }
+}
